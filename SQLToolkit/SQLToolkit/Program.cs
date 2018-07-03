4b8f55016c0e323b1ebddf186e8eefa78834caed
jackyzhou <jackyzhou@lean-soft.cn>
2018-07-03 16:38:08 +0800

    日志优化

--- SQLToolkit/SQLToolkit/Program.cs
+++ SQLToolkit/SQLToolkit/Program.cs
@@ -11,33 +11,41 @@ namespace SQLToolkit
     {
         static void Main(string[] args)
         {
-            Console.Write("===============BEGIN=================\n");
+            Log("===============BEGIN=================");
             var rootFolder = Directory.GetCurrentDirectory();
             var scriptsFolder = Path.Combine(rootFolder, "Scripts");
-            Console.Write("===============SQL=================\n");
-            Console.Write(string.Format("ScriptFolder:{0}\n",scriptsFolder));
+            Log("===============SQL=================");
+            Log(string.Format("ScriptFolder:{0}", scriptsFolder));
             var sqlFiles = Directory.GetFiles(scriptsFolder);
-            Console.Write(string.Format("Find {0} sql scripts\n", sqlFiles.Length));
+            Log(string.Format("Find {0} sql scripts", sqlFiles.Length));
             string sqlConnectionString = string.Format(@"Server={0},{1};Initial Catalog={2};Persist Security Info=False;User ID={3};Password={4};Connection Timeout=30;", args[0], args[1], args[2], args[3],args[4]);     
             SqlConnection conn = new SqlConnection(sqlConnectionString);
             Server server = new Server(new ServerConnection(conn));
             foreach (string file in sqlFiles)
             {
-                Console.Write(string.Format("Ready to exec sql script:{0}\n", file));
+                Log(string.Format("Ready to exec sql script:{0}", file));
+
                 try
                 {
                     server.ConnectionContext.ExecuteNonQuery(File.ReadAllText(file));
                 }
                 catch (Exception ex)
                 {
-                    Console.Write("Error:{0}\n", ex.ToString());
+                    Log(string.Format("Error:{0}", ex.ToString()));
                 }
-                Console.Write(string.Format("Finish to exec sql script:{0}\n", file));
+
+                Log(string.Format("Finish to exec sql script:{0}", file));
 
             }
 
             Console.Write("===============END=================\n");
 
+        }
+
+        static void Log(string message)
+        {
+            Console.Write(string.Format("{0}-{1}\n", DateTime.Now.ToString(), message));
+            
         }
     }
 }
