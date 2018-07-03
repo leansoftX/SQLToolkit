9ef7ce4ac3de02b57b44cf8369e5441a3ae50218
jackyzhou <jackyzhou@lean-soft.cn>
2018-07-03 15:17:00 +0800

    init project

---
+++ SQLToolkit/SQLToolkit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Common;
+
+
+namespace SQLToolkit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
