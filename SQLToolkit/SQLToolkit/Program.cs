fffbf8ececaa7b7f91c305f958e692e20e21dd57
jackyzhou <jackyzhou@lean-soft.cn>
2018-07-03 15:37:59 +0800

    支持sql单文件运行

--- SQLToolkit/SQLToolkit/Program.cs
+++ SQLToolkit/SQLToolkit/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.SqlServer.Management.Smo;
 using Microsoft.SqlServer.Management.Common;
+using System.IO;
+using System.Data.SqlClient;
 
 
 namespace SQLToolkit
@@ -9,7 +11,15 @@ namespace SQLToolkit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string sqlConnectionString = @"Server=labs-test.devopshub.cn,1333;Initial Catalog=devopslabs;Persist Security Info=False;User ID=sa;Password=P2ssw0rd;Connection Timeout=30;";
+
+            string script = File.ReadAllText(@"C:\SQLToolkit\SQLToolkit\SQLToolkit\Scripts\01-test.sql");
+         
+            SqlConnection conn = new SqlConnection(sqlConnectionString);
+
+            Server server = new Server(new ServerConnection(conn));
+
+            server.ConnectionContext.ExecuteNonQuery(script);
         }
     }
 }
