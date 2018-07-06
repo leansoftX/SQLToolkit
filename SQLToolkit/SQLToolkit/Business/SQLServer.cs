9ee24da3f8797880e5028c6d687b05d806d5ca2d
jackyzhou <jackyzhou@lean-soft.cn>
2018-07-06 18:24:27 +0800

    添加数据库还原命令

--- SQLToolkit/SQLToolkit/Business/SQLServer.cs
+++ SQLToolkit/SQLToolkit/Business/SQLServer.cs
@@ -24,5 +24,27 @@ namespace SQLToolkit.Business
 
             }
         }
+
+        public static void Restore(string database, string path)
+        {
+            try
+            {
+                string sql = string.Format(@"
+                ALTER DATABASE {0} SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+                GO
+                RESTORE DATABASE [{1}] 
+                FROM DISK = N'{2}' 
+                WITH FILE = 1, NOUNLOAD, REPLACE, STATS = 5", database, database, path);
+                Helper.SQLHelper.ExecuteNonQuery(sql);
+                Helper.LogHelper.Log(string.Format("Successfully RESTORE Database:{0} from {1}", database, path));
+
+            }
+            catch (Exception ex)
+            {
+                Helper.LogHelper.Log(string.Format("Error:{0}", ex.Message));
+
+            }
+        }
+
     }
 }
--- SQLToolkit/SQLToolkit/Program.cs
+++ SQLToolkit/SQLToolkit/Program.cs
@@ -27,16 +27,18 @@ namespace SQLToolkit
 
             command = args[0].ToLower();
 
-            if (command == "runscripts" || command == "backup")
+            if (command == "runscripts")
             {
-                if (command == "runscripts")
-                {
-                    RunScripts(args);
-                }
-                else if (command == "backup")
-                {
-                    Backup(args);
-                }
+                RunScripts(args);
+            }
+            else if (command == "backup")
+            {
+                Backup(args);
+            }
+            else if (command == "restore")
+            {
+                Restore(args);
+
             }
             else
             {
@@ -261,13 +263,120 @@ namespace SQLToolkit
 
 
 
-            SqlConnectionString = string.Format(@"Server={0};Initial Catalog={1};Persist Security Info=False;User ID={2};Password={3};Connection Timeout=30;", database_server, database_name, database_username, database_password);
+            SqlConnectionString = string.Format(@"Server={0};Initial Catalog={1};Persist Security Info=False;User ID={2};Password={3};Connection Timeout=30;", database_server, "master", database_username, database_password);
 
             //backup database before upgrade
             Business.SQLServer.Backup(database_name, backup_path);
 
         }
 
+
+        /// <summary>
+        /// backup database 
+        /// </summary>
+        private static void Restore(string[] args)
+        {
+            string database_server = string.Empty;
+            string database_name = string.Empty;
+            string database_username = string.Empty;
+            string database_password = string.Empty;
+            string backup_path = string.Empty;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "-s":
+                        if (i + 1 < args.Length)
+                        {
+                            database_server = args[i + 1];
+                        }
+                        else
+                        {
+                            Helper.LogHelper.Log(("Missing argument to -s.\n Supply argument as -s \"database server with port\"\n"));
+                            return;
+                        }
+                        break;
+                    case "-n":
+                        if (i + 1 < args.Length)
+                        {
+                            database_name = args[i + 1];
+                        }
+                        else
+                        {
+                            Helper.LogHelper.Log(("Missing argument to -n.\n Supply argument as -n \"database name\"\n"));
+                            return;
+                        }
+                        break;
+                    case "-u":
+                        if (i + 1 < args.Length)
+                        {
+                            database_username = args[i + 1];
+                        }
+                        else
+                        {
+                            Helper.LogHelper.Log(("Missing argument to -u.\n Supply argument as -u \"database username\"\n"));
+                            return;
+                        }
+                        break;
+                    case "-p":
+                        if (i + 1 < args.Length)
+                        {
+                            database_password = args[i + 1];
+                        }
+                        else
+                        {
+                            Helper.LogHelper.Log(("Missing argument to -p.\n Supply argument as -p \"database password\"\n"));
+                            return;
+                        }
+                        break;
+                    case "-path":
+                        if (i + 1 < args.Length)
+                        {
+                            backup_path = args[i + 1];
+                        }
+                        else
+                        {
+                            Helper.LogHelper.Log(("Missing argument to -path.\n Supply argument as -path \"backup path\"\n"));
+                            return;
+                        }
+                        break;
+                }
+            }
+            if (string.IsNullOrEmpty(database_server))
+            {
+                Console.Write(("Missing argument -s.\n Supply argument -s \"database server with port\"\n"));
+                return;
+            }
+            if (string.IsNullOrEmpty(database_name))
+            {
+                Console.Write(("Missing argument -n.\n Supply argument -n \"database name\"\n"));
+                return;
+            }
+            if (string.IsNullOrEmpty(database_username))
+            {
+                Console.Write(("Missing argument -u.\n Supply argument -u \"database username\"\n"));
+                return;
+            }
+            if (string.IsNullOrEmpty(database_password))
+            {
+                Console.Write(("Missing argument -p.\n Supply argument -p \"database password\"\n"));
+                return;
+            }
+            if (string.IsNullOrEmpty(backup_path))
+            {
+                Console.Write(("Missing argument -path.\n Supply argument -path \"backup path\"\n"));
+                return;
+            }
+
+
+
+            SqlConnectionString = string.Format(@"Server={0};Initial Catalog={1};Persist Security Info=False;User ID={2};Password={3};Connection Timeout=30;", database_server, "master", database_username, database_password);
+
+            //backup database before upgrade
+            Business.SQLServer.Restore(database_name, backup_path);
+
+        }
 
         /// <summary>
         /// Tool Instructions
